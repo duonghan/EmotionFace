74a207e0ed22cd17bbf08b8d26f3cfd91c9286ec
Duong Han <20155296@student.hust.edu.vn>
2018-04-25 01:36:08 +0700

    add splash activity

---
+++ app/src/main/java/vn/edu/hust/student/haicm/cognitiveldentify/SplashActivity.java
@@ -0,0 +1,27 @@
+package vn.edu.hust.student.haicm.cognitiveldentify;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(main);
+                finish();
+            }
+        },2000);
+    }
+}
